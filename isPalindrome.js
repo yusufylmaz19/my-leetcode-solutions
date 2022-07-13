96f6cea2d41d9082f7e846e7dd708366bf4977f0
yusuf yılmaz <yusufylmaz15@gmail.com>
2022-07-13 20:24:21 +0300

    ispalindrome

---
+++ isPalindrome.js
@@ -0,0 +1,8 @@
+let isPalindrome=(x)=>{
+    straight=x.toString();
+    reverse=straight.split('').reverse().join('');
+    if(straight===reverse){
+      return true;
+    }
+    else return false;
+  }
